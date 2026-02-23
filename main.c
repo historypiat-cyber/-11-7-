3cef9c7220834e8621351475856c9a4e65386359
historypiat-cyber <historypiat@gmail.com>
2026-02-23 11:50:43 +0200

    main.c

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>    //Пятницький Данило, 101 група, 7 лабораторна, 7 завдання.
-#include <limits.h>   
 #include <stdlib.h>
 int main()
 {
 
 int N,diff=0,size,g=0;
+while (1){
 printf("Enter N:\n");
 scanf("%d",&N);//Введення N
-if(N<0) return 0;//Перевірка
+if (N>0) break;
+}
 int *A = (int*)malloc(N * sizeof(int));
    
-   printf("A[0]=", A[0]);       //Введення першого елемента
-   scanf("%d", &A[0]);
-   for(int i=1;i<N;i++){        //Введення
+   for(int i=0;i<N;i++){        //Введення
        printf("\nA[%d]=", i);
        scanf("%d", &A[i]);
    }
@@ -22,15 +21,17 @@ int *A = (int*)malloc(N * sizeof(int));
           diff++;
       }
    }
-   size = diff;
-   int *B = (int*)malloc(size * sizeof(int));
+   int *B = (int*)malloc(diff * sizeof(int));
    for(int f=0;f<N;f++){
         if(A[f]>10){
             B[g]=A[f];
             g++;
         }
    }
-        for(int p=0;p<size;p++){
+   if (diff == 0){
+       printf("Масив порожній");
+   }
+        for(int p=0;p<diff;p++){
          printf("%d ",B[p]);
       }    
       free(A);
